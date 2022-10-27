79e88c6ffb57be72d2d03b9339757c2645315f8f
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-27 17:55:42 +0900

    スワイプ別の人になる

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Swipe;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -10,8 +11,14 @@ class UserController extends Controller
 
     public function index()
     {
-        // auth・userではないuserを1つ習得
-        $user = User::where('id', '<>', \Auth::user()->id)->first();
+        // すでにswipeしたuserを省いて、swipeしていないuserを1つ取得する
+
+        //すでに取得したuser・idsを取得
+        $swipeduserIds =  Swipe::where('from_user_id', \Auth::user()->id)->get()->pluck('to_user_id');
+
+        //swipeしていないuserを1一つ取得
+        $user = User::where('id', '<>', \Auth::user()->id)->whereNotIn('id', $swipeduserIds)->first();
+
         return view('pages.user.index', [
             'user' =>$user,
         ]);
